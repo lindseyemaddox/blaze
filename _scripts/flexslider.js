b0faf9eb4ef1cc439bbd0aacd9d8791c30bffe71
Lindsey Maddox <dev@lindseymaddox.com>
2018-09-21 19:44:27 -0500

    stretching out streaming icons, more removal of lindsey stuff, changing alt and title tags, etc.

--- _scripts/flexslider.js
+++ _scripts/flexslider.js
@@ -854,9 +854,9 @@
     asNavFor: "",                   //{NEW} Selector: Internal property exposed for turning the slider into a thumbnail navigation for another slider
 
     // Carousel Options
-    itemWidth: 100,                   //{NEW} Integer: Box-model width of individual carousel items, including horizontal borders and padding.
+    itemWidth: 40,                   //{NEW} Integer: Box-model width of individual carousel items, including horizontal borders and padding.
     itemMargin: 0,                  //{NEW} Integer: Margin between carousel items.
-    minItems: 1,                    //{NEW} Integer: Minimum number of carousel items that should be visible. Items will resize fluidly when below this.
+    minItems: 8,                    //{NEW} Integer: Minimum number of carousel items that should be visible. Items will resize fluidly when below this.
     maxItems: 8,                    //{NEW} Integer: Maxmimum number of carousel items that should be visible. Items will resize fluidly when above this limit.
     move: 0,                        //{NEW} Integer: Number of carousel items that should move on animation. If 0, slider will move all visible items.
 
